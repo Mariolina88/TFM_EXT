8bc47617908b8004a4d03ac41f8725c8ee1b02fd
Marialaura Bancheri <maryban@hotmail.it>
2019-09-25 11:57:05 +0200

    Range on parameters re-defined

--- src/main/java/monteCarlo/ReaderInputHydraulicParams.java
+++ src/main/java/monteCarlo/ReaderInputHydraulicParams.java
@@ -58,31 +58,31 @@ public class ReaderInputHydraulicParams {
 			if(i==j+1){
 			
 				residual_waterContent_VG=Double.parseDouble(line.split(csvSplitBy)[0]);
-				residual_waterContent_VG= residual_waterContent_VG<0?0:residual_waterContent_VG;
-				residual_waterContent_VG= residual_waterContent_VG>0.10?0.10:residual_waterContent_VG;
+				residual_waterContent_VG= residual_waterContent_VG<0?0.002:residual_waterContent_VG;
+				residual_waterContent_VG= residual_waterContent_VG>0.10?0.002:residual_waterContent_VG;
 				
 				saturated_waterContent_VG=Double.parseDouble(line.split(csvSplitBy)[1]);
-				saturated_waterContent_VG= saturated_waterContent_VG<0.2?0.3:saturated_waterContent_VG;
-				saturated_waterContent_VG= saturated_waterContent_VG>0.75?0.75:saturated_waterContent_VG;
+				saturated_waterContent_VG= saturated_waterContent_VG<0.2?0.380:saturated_waterContent_VG;
+				saturated_waterContent_VG= saturated_waterContent_VG>0.75?0.380:saturated_waterContent_VG;
 				
 				alpha_VG=Double.parseDouble(line.split(csvSplitBy)[2]);
-				alpha_VG=alpha_VG<0.01?0.02:alpha_VG;
-				alpha_VG=alpha_VG>0.5?0.5:alpha_VG;
+				alpha_VG=alpha_VG<0.01?0.027:alpha_VG;
+				alpha_VG=alpha_VG>0.5?0.027:alpha_VG;
 				
 								
 				n_VG=Double.parseDouble(line.split(csvSplitBy)[3]);
 				n_VG=n_VG<1.1?1.3:n_VG;
-				n_VG=n_VG>3?3:n_VG;
+				n_VG=n_VG>3?1.3:n_VG;
 				
 				
 				saturated_conductivity_VG=Double.parseDouble(line.split(csvSplitBy)[4]);
 				saturated_conductivity_VG=saturated_conductivity_VG<1?20:saturated_conductivity_VG;
-				saturated_conductivity_VG=saturated_conductivity_VG>1000?1000:saturated_conductivity_VG;
+				saturated_conductivity_VG=saturated_conductivity_VG>1000?20:saturated_conductivity_VG;
 				
 				
 				tau_VG=Double.parseDouble(line.split(csvSplitBy)[5]);
-				tau_VG=tau_VG<-30?-30:tau_VG;
-				tau_VG=tau_VG>30?30:tau_VG;
+				tau_VG=tau_VG<-20?-0.106:tau_VG;
+				tau_VG=tau_VG>30?-0.106:tau_VG;
 			}
 
 
